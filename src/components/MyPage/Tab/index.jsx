91994bed78ed3596b661d50e582c26d0fee4897c
marchfirst01 <marchfirst3113@gmail.com>
2024-07-05 00:01:29 +0900

    feat: my page

---
+++ src/components/MyPage/Tab/index.jsx
@@ -0,0 +1,46 @@
+import font from '@/styles/font';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export default function Tab() {
+  const [selected, setSelected] = useState('my');
+  return (
+    <Wrapper>
+      <Button selected={selected === 'my'} onClick={() => setSelected('my')}>
+        내가 올린 스팟
+      </Button>
+      <Line />
+      <Button
+        selected={selected === 'save'}
+        onClick={() => setSelected('save')}
+      >
+        내가 저장한 스팟
+      </Button>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Line = styled.div`
+  width: 1px;
+  height: 4.2rem;
+  border-right: 1px solid #c2c2c2;
+`;
+
+const Button = styled.div`
+  ${font.bold_16}
+  width: 50%;
+  text-align: center;
+  padding: 1.1rem 0;
+
+  cursor: pointer;
+
+  border-top: ${props => props.selected && '1px solid #c2c2c2'};
+  border-bottom: 1px solid #c2c2c2;
+`;
