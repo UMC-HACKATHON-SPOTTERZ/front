34c379f7b24cb342fe7c6204627a464e57db93b7
marchfirst01 <marchfirst3113@gmail.com>
2024-07-04 21:36:07 +0900

    design: home 화면 기본 디자인

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,47 @@
 import Button from "@/components/common/Button";
-import SpotList from "@/components/common/SpotList";
+import color from "@/styles/color";
+import font from "@/styles/font";
+import Image from "next/image";
+import styled from "styled-components";
 
 export default function Home() {
   return (
-    <>
-      <p>main page</p>
+    <Wrapper>
+      <Text>인생샷 명소, 제일 쉽게 찾는 법</Text>
+      <Image src='/icons/logo.svg' alt="logo" priority={true} width={194} height={50} />
       <Button>내 주변 포토스팟 찾기</Button>
-      <SpotList/>
-    </>
+      <Button>마이페이지</Button>
+      <LinkWrapper>
+        <Link href="/">로그인</Link>
+        /
+        <Link href="/">회원가입</Link>
+      </LinkWrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.2rem;
+
+  height: 100vh;
+`
+
+const Text = styled.p`
+  color: ${color.primary};
+  ${font.medium_16};
+`
+
+const LinkWrapper = styled.div`
+  color: ${color.primary_100};
+  ${font.regular_14};
+  text-decoration: underline;
+`
+
+const Link = styled.a`
+  color: ${color.primary_100};
+  ${font.regular_14};
+`
--- src/styles/color.js
+++ src/styles/color.js
@@ -1,6 +1,7 @@
 const color = {
-  primary: 'rgba(105, 210, 255)',
+  primary: '#69D2FF',
   primary_50: 'rgba(105, 210, 255, 0.55)',
+  primary_100: '#00B3FF',
 };
 
 export default color;
